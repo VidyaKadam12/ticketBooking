fbc7e29af8a511cdd60a94dbd10df73f42b5fc92
vcxzsertyj <kvidya8650@gmail.com>
2026-01-16 14:58:55 +0530

    checking

--- src/main/java/com/ticketBooking/app/AppApplication.java
+++ src/main/java/com/ticketBooking/app/AppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("holaa");
+
 		SpringApplication.run(AppApplication.class, args);
 	}
 
